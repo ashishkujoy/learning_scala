0d63109f21ee7c0c079eca69c0ff998513e77cf5
ashishJoy <ashishkujoy@gmail.com>
2018-10-14 09:50:16 +0530

    [Ashish @Streams] Add ability to add create new group or add a device to an existing group

--- streams-basics/src/main/scala/com/thoughtworks/streams/actors/DeviceManager.scala
+++ streams-basics/src/main/scala/com/thoughtworks/streams/actors/DeviceManager.scala
@@ -1,10 +1,47 @@
 package com.thoughtworks.streams.actors
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, Props, Terminated}
+import com.thoughtworks.streams.actors.DeviceManager.{ReplyGroupList, RequestGroupList, RequestTrackDevice}
+import com.thoughtworks.streams.actors.ImplicitConverters._
 
 object DeviceManager {
   final case class RequestTrackDevice(groupId: String, deviceId: String)
   final case class UnknownGroupOrDeviceId(groupId: String, deviceId: String)
+  final case class RequestGroupList(requestId: Long)
+  final case class ReplyGroupList(requestId: Long, ids: Set[String])
   case object DeviceRegistered
+
+  def props(): Props = Props(new DeviceManager)
+
 }
 
-class DeviceManager extends Actor { override def receive: Receive = ??? }
+class DeviceManager extends Actor {
+  private var groupIdToActor = Map.empty[String, ActorRef]
+
+  override def receive: Receive = {
+    case trackMsg @ RequestTrackDevice(_, _) => handleValidRequestTrackDevice(trackMsg)
+    case Terminated(groupActor)              => removeDeadActor(groupActor)
+    // TODO: Find a way to test this functionality without breaking encapsulation
+    case RequestGroupList(requestId) => sender() ! ReplyGroupList(requestId, groupIdToActor.keys.toSet)
+  }
+
+  private def removeDeadActor(groupActor: ActorRef): Unit = {
+    val mayBeGroupId = groupIdToActor.findKeyByValue(groupActor)
+    mayBeGroupId.foreach(groupIdToActor -= _)
+  }
+
+  private def handleValidRequestTrackDevice(trackMsg: RequestTrackDevice): Unit = {
+    val groupId = trackMsg.groupId
+    groupIdToActor.get(groupId) match {
+      case Some(ref) => ref forward trackMsg
+      case None      => createNewGroup(groupId) forward trackMsg
+    }
+
+  }
+  private def createNewGroup(groupId: String): ActorRef = {
+    val groupActor = context.actorOf(DeviceGroup.props(groupId), "group-" + groupId)
+    context.watch(groupActor)
+    groupIdToActor += groupId -> groupActor
+    groupActor
+  }
+
+}
---
+++ streams-basics/src/test/scala/com/thoughtworks/streams/actors/DeviceManagerTest.scala
@@ -0,0 +1,59 @@
+package com.thoughtworks.streams.actors
+import akka.actor.{ActorRef, ActorSystem}
+import akka.testkit.{TestKit, TestProbe}
+import com.thoughtworks.streams.actors.DeviceManager._
+import org.scalatest._
+
+import scala.concurrent.duration._
+
+class DeviceManagerTest(_system: ActorSystem)
+    extends TestKit(_system)
+    with Matchers
+    with WordSpecLike
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach {
+
+  private val groupId        = "group"
+  private val deviceId       = "device"
+  private val anotherGroupId = "group2"
+
+  private var deviceManager: ActorRef = _
+  private var probe: TestProbe        = _
+
+  def this() = this(ActorSystem("DeviceTest"))
+
+  override protected def afterAll(): Unit = system.terminate()
+
+  override protected def beforeEach(): Unit = {
+    deviceManager = system.actorOf(DeviceManager.props())
+    probe = TestProbe()
+  }
+
+  "Device Manager" should {
+    "add new device actors to a group actor" in {
+      deviceManager.tell(RequestTrackDevice(groupId, deviceId), probe.ref)
+      probe.expectMsg(DeviceRegistered)
+
+      deviceManager.tell(RequestTrackDevice(groupId, "device2"), probe.ref)
+      probe.expectMsg(DeviceRegistered)
+    }
+
+    "give all active group actors" in {
+      deviceManager.tell(RequestGroupList(21), probe.ref)
+      probe.expectMsg(3000.millisecond, ReplyGroupList(21, Set.empty))
+
+      deviceManager.tell(RequestTrackDevice(groupId, deviceId), probe.ref)
+      probe.expectMsg(DeviceRegistered)
+
+      deviceManager.tell(RequestGroupList(21), probe.ref)
+      probe.expectMsg(3000.millisecond, ReplyGroupList(21, Set(groupId)))
+
+      deviceManager.tell(RequestTrackDevice(anotherGroupId, deviceId), probe.ref)
+      probe.expectMsg(DeviceRegistered)
+
+      deviceManager.tell(RequestGroupList(21), probe.ref)
+      probe.expectMsg(3000.millisecond, ReplyGroupList(21, Set(groupId, anotherGroupId)))
+    }
+
+  }
+}
