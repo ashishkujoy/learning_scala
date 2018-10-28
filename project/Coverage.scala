8d812c95995fd25194af241dfbd60b7e48653a72
ashishJoy <ashishkujoy@gmail.com>
2018-10-28 19:01:53 +0530

    [Ashish @project_setup] Update coverage plugin settings

--- project/Coverage.scala
+++ project/Coverage.scala
@@ -1,8 +1,13 @@
+import org.scoverage.coveralls.Imports.CoverallsKeys._
 import sbt.{Def, _}
+
+import scala.util.Properties
+
 object Coverage extends AutoPlugin {
-//  import scoverage.ScoverageSbtPlugin.autoImport._
+  import scoverage.ScoverageSbtPlugin.autoImport._
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
-//    coverageMinimum := 50,
-//    coverageFailOnMinimum := false
+    coverallsToken := Some(Properties.envOrElse("COVERALLS_REPO_TOKEN", "")),
+    coverageMinimum := 20,
+    coverageFailOnMinimum := false
   )
 }
