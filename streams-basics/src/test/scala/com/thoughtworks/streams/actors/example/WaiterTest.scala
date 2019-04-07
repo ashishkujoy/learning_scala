9c31df4a9f9b4e13a2ffa3883a1474ac9fd15572
ashish kumar <ashishkujoy@gmail.com>
2019-04-07 22:27:59 +0530

    Fix failing waiter test

--- streams-basics/src/test/scala/com/thoughtworks/streams/actors/example/WaiterTest.scala
+++ streams-basics/src/test/scala/com/thoughtworks/streams/actors/example/WaiterTest.scala
@@ -1,10 +1,11 @@
 package com.thoughtworks.streams.actors.example
 
 import akka.actor.{ActorRef, ActorSystem}
+import akka.testkit.ImplicitSender
 import com.thoughtworks.streams.actors.example.Waiter.OrderAcknowledgment
 import com.thoughtworks.streams.actors.iot.ActorsTest
 
-class WaiterTest extends ActorsTest(ActorSystem("WaiterTest")) {
+class WaiterTest extends ActorsTest(ActorSystem("Waiter-Test")) with ImplicitSender{
   private var waiter: ActorRef = _
   private val RajmaChawal      = "RajmaChawal"
 
