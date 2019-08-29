00540a4ad1180b7a1dce55d81517b507becfcc97
ashish kumar <ashishkujoy@gmail.com>
2019-08-29 22:48:11 +0530

    [Ashish @Streams] Adds source and sink created from open graph

---
+++ streams-basics/src/main/scala/com/thoughtworks/streams/playground/OpenGraphs.scala
@@ -0,0 +1,47 @@
+package com.thoughtworks.streams.playground
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, SinkShape, SourceShape}
+import akka.stream.scaladsl.{Broadcast, Concat, GraphDSL, Sink, Source}
+
+object OpenGraphs {
+
+  def main(args: Array[String]): Unit = {
+    implicit val actorSystem: ActorSystem = ActorSystem("OpenGraph")
+    implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
+
+    complexSource.runForeach(println)
+    complexSource.to(complexSink).run()
+  }
+
+  /*Sends element of both first and second source but first send all elements of source1*/
+  def complexSource: Source[Int, NotUsed] = {
+    val firstSource  = Source(1 to 10)
+    val secondSource = Source(100 to 200)
+
+    Source.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val concat = builder.add(Concat[Int](2))
+
+      firstSource ~> concat
+      secondSource ~> concat
+
+      SourceShape(concat.out)
+    })
+  }
+  /*Sends incoming element to both first and second sink*/
+  def complexSink: Sink[Int, NotUsed] = {
+    val firstSink = Sink.foreach[Int]( a => println(s"Sink1 $a"))
+    val secondSink = Sink.foreach[Int]( a => println(s"Sink2 $a"))
+
+    Sink.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val broadcast = builder.add(Broadcast[Int](2))
+      broadcast ~> firstSink
+      broadcast ~> secondSink
+      SinkShape(broadcast.in)
+    })
+  }
+}
