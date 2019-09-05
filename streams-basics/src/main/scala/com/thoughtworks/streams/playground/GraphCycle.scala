2aa95fe25273c18d5dc1f0759795e3e1d20a60eb
ashish kumar <ashishkujoy@gmail.com>
2019-09-05 20:30:30 +0530

    [Ashish @Streams] Adds a fibonacci graph

--- streams-basics/src/main/scala/com/thoughtworks/streams/playground/GraphCycle.scala
+++ streams-basics/src/main/scala/com/thoughtworks/streams/playground/GraphCycle.scala
@@ -2,8 +2,9 @@ package com.thoughtworks.streams.playground
 
 import akka.NotUsed
 import akka.stream.scaladsl._
-import akka.stream.{ClosedShape, Graph, OverflowStrategy}
+import akka.stream.{ClosedShape, Graph, OverflowStrategy, UniformFanInShape}
 import com.thoughtworks.streams.Implicits
+import scala.concurrent.duration._
 
 object GraphCycle extends Implicits {
   private val acceleratorWithCyclicDeadLock: Graph[ClosedShape.type, NotUsed] = GraphDSL.create() { implicit builder =>
@@ -52,7 +53,38 @@ object GraphCycle extends Implicits {
     ClosedShape
   }
 
+  private val fibonacciShape: Graph[UniformFanInShape[BigInt, BigInt], NotUsed] = GraphDSL.create() { implicit builder =>
+    val zipShape                      = builder.add(Zip[BigInt, BigInt]())
+    val seedElementProviderShape      = builder.add(MergePreferred[(BigInt, BigInt)](1))
+    val newSeedElementCalculatorShape = builder.add(Flow[(BigInt, BigInt)].map(a => (a._2, a._1 + a._2)).throttle(5 , 1 seconds))
+    val newSeedElementBroadcast       = builder.add(Broadcast[(BigInt, BigInt)](2))
+    val fibonacciNumberExtractorShape = builder.add(Flow[(BigInt, BigInt)].map(_._1))
+
+    import GraphDSL.Implicits._
+
+    zipShape.out ~> seedElementProviderShape ~> newSeedElementCalculatorShape ~> newSeedElementBroadcast ~> fibonacciNumberExtractorShape
+    seedElementProviderShape.preferred <~ newSeedElementBroadcast
+
+    UniformFanInShape(fibonacciNumberExtractorShape.out, zipShape.in0, zipShape.in1)
+  }
+
+  private val fibonacciGraph: RunnableGraph[NotUsed] = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
+    val firstElementSourceShape  = builder.add(Source.single[BigInt](1))
+    val secondElementSourceShape = builder.add(Source.single[BigInt](1))
+    val fiboShape                = builder.add(fibonacciShape)
+    val printerSinkShape         = builder.add(Sink.foreach(println))
+
+    import GraphDSL.Implicits._
+
+    firstElementSourceShape ~> fiboShape
+    secondElementSourceShape ~> fiboShape
+    fiboShape ~> printerSinkShape
+
+    ClosedShape
+  })
+
   def main(args: Array[String]): Unit = {
-    RunnableGraph.fromGraph(repeaterWithBuffer).run()
+//    RunnableGraph.fromGraph(repeaterWithBuffer).run()
+    fibonacciGraph.run()
   }
 }
