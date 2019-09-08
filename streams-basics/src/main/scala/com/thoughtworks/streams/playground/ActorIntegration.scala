3c58397d7b1db6f18ea27950e0e3423d6879f69c
ashish kumar <ashishkujoy@gmail.com>
2019-09-08 20:01:39 +0530

    [Ashish @Streams] Adds a actor powered sink

--- streams-basics/src/main/scala/com/thoughtworks/streams/playground/ActorIntegration.scala
+++ streams-basics/src/main/scala/com/thoughtworks/streams/playground/ActorIntegration.scala
@@ -19,16 +19,44 @@ object ActorIntegration extends Implicits {
         sender() ! (2 * integer)
     }
   }
+
+  class SimpleSinkActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case StreamInit =>
+        log.info("Stream initialize")
+        sender ! StreamAck
+      case StreamComplete   => log.info("Stream complete")
+      case StreamFailed(ex) => log.error(ex.toString)
+      case message =>
+        log.info(s"Receive a message $message")
+        sender ! StreamAck
+    }
+  }
+
+  case object StreamInit
+  case object StreamAck
+  case object StreamComplete
+  case class StreamFailed(throwable: Throwable)
+
   private val simpleActor: ActorRef                         = actorSystem.actorOf(Props(new SimpleActor), "SimpleActor")
   private implicit val timeout: Timeout                     = Timeout(2 seconds)
   private val actorPoweredFlow: Flow[Int, Int, NotUsed]     = Flow[Int].ask[Int](4)(simpleActor)
   private val source: Source[Int, NotUsed]                  = Source(1 to 10)
   private val actorPoweredSource: Source[Nothing, ActorRef] = Source.actorRef(5, OverflowStrategy.dropNew)
+  private val sinkActor                                     = actorSystem.actorOf(Props(new SimpleSinkActor()), "SimpleSinkActor")
+  private val actorPoweredSink: Sink[Any, NotUsed] = Sink.actorRefWithAck(
+    ref = sinkActor,
+    onInitMessage = StreamInit,
+    ackMessage = StreamAck,
+    onCompleteMessage = StreamComplete,
+    onFailureMessage = throwable => StreamFailed(throwable)
+  )
 
   def main(args: Array[String]): Unit = {
 //    source.via(actorPoweredFlow).to(Sink.ignore).run()
-    val materialisedActor = actorPoweredSource.to(Sink.foreach[Int](int => println(s"receive int $int"))).run()
+//    val materialisedActor = actorPoweredSource.to(Sink.foreach[Int](int => println(s"receive int $int"))).run()
 
-    Seq(1, 2, 3, 4, 5, 56, 7, 8, 9, 0).foreach(materialisedActor ! _)
+//    Seq(1, 2, 3, 4, 5, 56, 7, 8, 9, 0).foreach(materialisedActor ! _)
+    Source(1 to 10).to(actorPoweredSink).run()
   }
 }
